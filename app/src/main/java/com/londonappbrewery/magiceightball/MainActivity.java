c0a800c77a628075d99b26abf3a0f049800df59a
Karthik <karthik.08jul@gmail.com>
2019-12-13 00:25:21 -0600

    Magic 8 Ball - first push.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,42 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button askButton;
+    ImageView displayImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        askButton = (Button)findViewById(R.id.askButton);
+        displayImage = (ImageView)findViewById(R.id.image_eightBall);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int [] buttonArray = {R.drawable.ball1, R.drawable.ball2, R.drawable.ball3,
+                        R.drawable.ball4, R.drawable.ball5};
+
+                Random randNumGen = new Random();
+                int randomNumber = randNumGen.nextInt(5);
+                displayImage.setImageResource(buttonArray[randomNumber]);
+
+            }
+        });
+
+
+
+
+
+
     }
 }
